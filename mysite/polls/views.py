2aaa21f619cc0e550ac2e6b16b6f39ed59637288
bananalytics <31481575+bananalytics@users.noreply.github.com>
2017-11-22 10:23:37 +0100

    .

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
+
 
 from .models import Choice, Question
 
@@ -57,6 +59,13 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 # def results(request, question_id):
 #     response = "You're looking at the results of question %s."
